0c11300d36ab06fccd2c13a5de93e926844588fc
ExactExampl <exactxmpl@protonmail.com>
2024-07-08 03:09:50 +0000

    fixup! RESTRICT AUTOMERGE FRP bypass defense in App battery usage page
    Change-Id: I431d0442f2f39bde283d028deba8c3df734a9ad2

--- tests/robotests/src/com/android/settings/fuelgauge/AdvancedPowerUsageDetailTest.java
+++ tests/robotests/src/com/android/settings/fuelgauge/AdvancedPowerUsageDetailTest.java
@@ -822,9 +822,4 @@ public class AdvancedPowerUsageDetailTest {
     public void shouldSkipForInitialSUW_returnTrue() {
         assertThat(mFragment.shouldSkipForInitialSUW()).isTrue();
     }
-
-    @Test
-    public void shouldSkipForInitialSUW_returnTrue() {
-        assertThat(mFragment.shouldSkipForInitialSUW()).isTrue();
-    }
 }
