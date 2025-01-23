f71d6a4e3b97eb0607051c10bfaa11dc619b4cac
ExactExampl <xmpl@dontsleep.work>
2025-01-23 20:21:14 +0200

    Settings: make smooth display summary show actual min refresh rate
    Change-Id: I4174dbe00422c7aaccbbe13a9aaaf13684f44ba3

--- src/com/android/settings/display/PeakRefreshRatePreferenceController.java
+++ src/com/android/settings/display/PeakRefreshRatePreferenceController.java
@@ -89,10 +89,16 @@ public class PeakRefreshRatePreferenceController extends TogglePreferenceControl
         super.displayPreference(screen);
 
         int defaultPeakRefreshRate = Math.round(mPeakRefreshRate);
-
+        int minRefreshRate = Math.round(Settings.System.getFloat(
+                mContext.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 60.00f));
+        String preferenceSummary = mContext.getString(
+                R.string.peak_refresh_rate_summary,
+                defaultPeakRefreshRate).replace("60",
+                Integer.toString(minRefreshRate));
+            
         mPreference = screen.findPreference(getPreferenceKey());
-        mPreference.setSummary(mContext.getString(
-                R.string.peak_refresh_rate_summary, defaultPeakRefreshRate));
+        mPreference.setSummary(preferenceSummary);
     }
 
     @Override
